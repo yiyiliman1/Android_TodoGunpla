90b3decf7aac893973b18321d58e7e6ca67a339f
yiyiliman1 <yiyiliman113@gmail.com>
2025-02-06 14:19:48 +0100

    enlaces

--- app/src/main/java/com/example/tiendagunpla/inicio/PaginaInicial.java
+++ app/src/main/java/com/example/tiendagunpla/inicio/PaginaInicial.java
@@ -1,6 +1,7 @@
 package com.example.tiendagunpla.inicio;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -80,6 +81,34 @@ public class PaginaInicial extends AppCompatActivity {
                 Intent intent = new Intent(PaginaInicial.this, PintarInicio.class);
                 startActivity(intent);
             }
+        });
+
+        ImageView ToInst = findViewById(R.id.Insta);
+        ToInst.setOnClickListener(v -> {
+            String url = "https://www.instagram.com/gundamstagram/?hl=es";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
+
+        ImageView ToTw = findViewById(R.id.Tw);
+        ToTw.setOnClickListener(v -> {
+            String url = "https://x.com/GundamSpain";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
+
+        ImageView ToYT = findViewById(R.id.YT);
+        ToYT.setOnClickListener(v -> {
+            String url = "https://www.youtube.com/@MiniFLY/videos";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
+
+        TextView ToPri = findViewById(R.id.Pri);
+        ToPri.setOnClickListener(v -> {
+            String url = "https://policies.google.com/privacy?hl=es";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
         });
     }
 
